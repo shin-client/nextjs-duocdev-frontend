cf878f64bbcff2356ce57eed2a0d485db7793be5
ngocshintrannn <ngoc.shintrann@gmail.com>
2025-07-28 16:14:34 +0700

    feat: Refactor code structure for improved readability and maintainability

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [new URL("https://4kwallpapers.com/**")],
+  },
 };
 
 export default nextConfig;
