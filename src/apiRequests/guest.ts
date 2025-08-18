45e4c6f710cde6be24d2987cb4e2cb708d45a93e
ngocshintrannn <ngoc.shintrann@gmail.com>
2025-08-18 16:22:17 +0700

    feat: implement guest authentication API with login, logout, and refresh token functionality, and create guest login form component

---
+++ src/apiRequests/guest.ts
@@ -0,0 +1,51 @@
+import http from "@/lib/http";
+import {
+  LogoutBodyType,
+  RefreshTokenBodyType,
+  RefreshTokenResType,
+} from "@/schemaValidations/auth.schema";
+import { MessageResType } from "@/schemaValidations/common.schema";
+import {
+  GuestLoginBodyType,
+  GuestLoginResType,
+} from "@/schemaValidations/guest.schema";
+
+const guestApiRequest = {
+  refreshTokenRequest: null as Promise<{
+    status: number;
+    payload: RefreshTokenResType;
+  }> | null,
+  sLogin: (body: GuestLoginBodyType) =>
+    http.post<GuestLoginResType>("/guest/auth/login", body),
+  login: (body: GuestLoginBodyType) =>
+    http.post<GuestLoginResType>("/api/guest/auth/login", body, { baseUrl: "" }),
+  sLogout: (body: LogoutBodyType & { accessToken: string }) =>
+    http.post<MessageResType>(
+      "/guest/auth/logout",
+      { refreshToken: body.refreshToken },
+      {
+        headers: {
+          Authorization: `Bearer ${body.accessToken}`,
+        },
+      },
+    ),
+  logout: () =>
+    http.post<MessageResType>("/api/guest/auth/logout", null, { baseUrl: "" }),
+  sRefreshToken: (body: RefreshTokenBodyType) =>
+    http.post<RefreshTokenResType>("/guest/auth/refresh-token", body),
+  async refreshToken() {
+    if (this.refreshTokenRequest) {
+      return this.refreshTokenRequest;
+    }
+    this.refreshTokenRequest = http.post<RefreshTokenResType>(
+      "/api/guest/auth/refresh-token",
+      null,
+      { baseUrl: "" },
+    );
+    const result = await this.refreshTokenRequest;
+    this.refreshTokenRequest = null;
+    return result;
+  },
+};
+
+export default guestApiRequest;
---
+++ src/app/api/auth/guest/login/route.ts
@@ -0,0 +1,42 @@
+import guestApiRequest from "@/apiRequests/guest";
+import { HttpError } from "@/lib/http";
+import { GuestLoginBodyType } from "@/schemaValidations/guest.schema";
+import { decode } from "jsonwebtoken";
+import { cookies } from "next/headers";
+
+export async function POST(request: Request) {
+  const body = (await request.json()) as GuestLoginBodyType;
+  const cookieStore = await cookies();
+
+  try {
+    const { payload } = await guestApiRequest.sLogin(body);
+    const { accessToken, refreshToken } = payload.data;
+    const decodedAccessToken = decode(accessToken) as { exp: number };
+    const decodedRefreshToken = decode(refreshToken) as { exp: number };
+    cookieStore.set({
+      name: "accessToken",
+      value: accessToken,
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      secure: true,
+      expires: decodedAccessToken.exp * 1000,
+    });
+    cookieStore.set({
+      name: "refreshToken",
+      value: refreshToken,
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      secure: true,
+      expires: decodedRefreshToken.exp * 1000,
+    });
+    return Response.json(payload);
+  } catch (error) {
+    if (error instanceof HttpError) {
+      return Response.json(error.payload, { status: error.status });
+    } else {
+      return Response.json({ message: "Có lỗi xảy ra" }, { status: 500 });
+    }
+  }
+}
