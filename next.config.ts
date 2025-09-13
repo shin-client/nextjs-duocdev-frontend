780287322e9f1f25823e0ac8260748fdc11124b7
ngocshintrannn <ngoc.shintrann@gmail.com>
2025-09-13 13:45:34 +0700

    feat: add validation message

--- next.config.ts
+++ next.config.ts
@@ -10,5 +10,9 @@ const nextConfig: NextConfig = {
   },
 };
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin({
+  experimental: {
+    createMessagesDeclaration: "./messages/en.json",
+  },
+});
 export default withNextIntl(nextConfig);
