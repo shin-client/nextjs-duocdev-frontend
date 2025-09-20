ab2fa345ef073823fce28057ec94eb4bc0fd43dc
ngocshintrannn <ngoc.shintrann@gmail.com>
2025-09-20 11:24:05 +0700

    feat: change static id to slug

--- src/app/[locale]/(public)/dishes/[slug]/page.tsx
+++ src/app/[locale]/(public)/dishes/[slug]/page.tsx
@@ -1,15 +1,17 @@
 import dishApiRequest from "@/apiRequests/dish";
-import { getIdFromSlugUrl, wrapServerApi } from "@/lib/utils";
+import { generateSlugUrl, getIdFromSlugUrl, wrapServerApi } from "@/lib/utils";
 import DishDetail from "./dish-detail";
+import { notFound } from "next/navigation";
 
 interface Props {
   params: Promise<{ slug: string }>;
 }
 
+// TODO: fix chỉ lấy những món phổ biến!
 export async function generateStaticParams() {
   const res = await dishApiRequest.list();
-  return res.payload.data.map((dish: { id: number }) => ({
-    id: dish.id.toString(),
+  return res.payload.data.map((dish: { name: string; id: number }) => ({
+    slug: generateSlugUrl({ name: dish.name, id: dish.id }),
   }));
 }
 
@@ -19,6 +21,10 @@ const DishPage = async ({ params }: Props) => {
 
   const data = await wrapServerApi(() => dishApiRequest.getDish(id));
   const dish = data?.payload.data;
+
+  if (!dish) {
+    notFound();
+  }
 
   return <DishDetail dish={dish} />;
 };
